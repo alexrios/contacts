e1d805a4d6cceb1a85034cfb4521fb5f5e781452
Alex Rios <alex.rios1337@gmail.com>
2018-03-06 16:10:03 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
+
+class App extends Component {
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then( (contacts) => {
+      this.setState({contacts})
+    })
+  }
+
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+    ContactsAPI.remove(contact)
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact}/>
+      </div>
+    );
+  }
+}
+
+export default App;
